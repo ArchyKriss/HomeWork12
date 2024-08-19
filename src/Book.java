cc60f1a76daec3854f03b064fd851df24119e0b4
user <git@github.com:ArchyKris/helloWord.git>
2024-08-19 21:41:02 +0300

    Добавлены условия из второй задачи

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private Author author;
@@ -21,4 +23,25 @@ public class Book {
         this.bookYearRelease = bookYearRelease;
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", author=" + author +
+                ", bookYearRelease=" + bookYearRelease +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return bookYearRelease == book.bookYearRelease && Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, author, bookYearRelease);
+    }
 }
